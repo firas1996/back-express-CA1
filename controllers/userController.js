f7b805dde017105fc00ae9fc32bfac64b591eaff
firas1996 <firas.hm.1996@gmail.com>
2026-02-03 19:24:21 +0100

    aa

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,3 +45,20 @@ exports.getUserById = async (req, res) => {
     });
   }
 };
+
+exports.updateUserById = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json({
+      message: "User Updated !!!",
+      data: user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Error",
+      error: error,
+    });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,13 @@ const {
   createUser,
   getUsers,
   getUserById,
+  updateUserById,
 } = require("../controllers/userController");
 const router = express.Router();
 
 router.post("/createUser", createUser);
 router.get("/getUsers", getUsers);
 router.get("/getUser/:id", getUserById);
+router.patch("/updateUser/:id", updateUserById);
 
 module.exports = router;
