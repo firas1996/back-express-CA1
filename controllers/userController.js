2d89cf20f42b1395efc53e006f9a7b3117779be9
firas1996 <firas.hm.1996@gmail.com>
2026-02-03 18:37:15 +0100

    a

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,11 +2,26 @@ const User = require("../models/userModel");
 
 exports.createUser = async (req, res) => {
   try {
-    console.log(req.body);
     const newUser = await User.create(req.body);
     res.status(201).json({
       message: "User createdd !!!",
       data: newUser,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Error",
+      error: error,
+    });
+  }
+};
+
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      message: "Users fetched !!!",
+      nbr: users.length,
+      data: users,
     });
   } catch (error) {
     res.status(400).json({
